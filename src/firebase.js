b69f7fe5fd647a53fa8379eaef0f4c24f1cffeac
asolyma <asolyma@hotmail.com>
2021-09-11 21:27:29 +0200

    fixed some css with the input field padding,margin-left

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,12 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore/lite";
 import { getAuth } from "firebase/auth";
-
 const firebaseConfig = {
-  apiKey: "AIzaSyCb2ZDaLcVDJA-x4qz93SwlkpU-iKNGJyw",
-  authDomain: "linkedin-clone-80070.firebaseapp.com",
-  projectId: "linkedin-clone-80070",
-  storageBucket: "linkedin-clone-80070.appspot.com",
-  messagingSenderId: "850981619676",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: "1:850981619676:web:6186bc606dc186562bb119",
 };
 
